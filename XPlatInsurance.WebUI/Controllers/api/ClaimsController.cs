d5f613fc85f73956a5270c3521d7423d9b29f0d3
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2014-11-14 11:50:55 -0800

    Updated to use in memory data store

--- XPlatInsurance.WebUI/Controllers/api/ClaimsController.cs
+++ XPlatInsurance.WebUI/Controllers/api/ClaimsController.cs
@@ -13,86 +13,85 @@ namespace XPlatInsurance.WebUI.Controllers.api
   public class ClaimsController : ApiController
   {
 
-    XPlatInsuranceEntities ctx;
+    public static List<Claim> _MyClaims = new List<Claim>() {
+      new Claim {
+        ClaimID=1, 
+        CustomerID="4567",
+        IncidentDate=new DateTime(2014,11,12),
+        ReportDateTimeUtc = DateTime.UtcNow,
+        Location = "Mandalay Bay parking lot",
+        StatusID = 1,
+        ClaimDetails = new List<ClaimDetail> {
+          new ClaimDetail() {
+            ClaimDetailID = 1,
+            ClaimID=1234,
+            VehicleID = "Blue Subaru Sedan",
+            Damage = "Cracked Windshield"
+          }
+        }
+      }
+    };
+
 
     public ClaimsController()
     {
-      ctx = new Models.XPlatInsuranceEntities();
     }
     
     // GET api/<controller>
     public IEnumerable<Models.Claim> Get()
     {
-      return ctx.Claims;
+      return _MyClaims;
     }
 
     // GET api/<controller>/5
     public Models.Claim Get(int id)
     {
-      return ctx.Claims.Include("ClaimDetails").First(c => c.ClaimID == id);
+      return _MyClaims.First(c => c.ClaimID==id);
     }
 
     // POST api/<controller>
     public void Post([FromBody]Models.Claim value)
     {
-      ctx.Claims.Add(value);
-      ctx.SaveChanges();
+      _MyClaims.Add(value);
     }
 
     // PUT api/<controller>/5
     public void Put(int id, [FromBody]Models.Claim value)
     {
-      ctx.Entry(value).State = EntityState.Modified;
-      ctx.SaveChanges();
+      _MyClaims.Remove(_MyClaims.First(c => c.ClaimID == id));
+      _MyClaims.Add(value);
     }
 
     // DELETE api/<controller>/5
     public void Delete(int id)
     {
-      ctx.Claims.Remove(ctx.Claims.Find(id));
+      _MyClaims.Remove(_MyClaims.First(c => c.ClaimID == id));
     }
   }
 
   public class ClaimDetailsController : ApiController
   {
 
-    XPlatInsuranceEntities ctx;
-
     public ClaimDetailsController()
     {
-      ctx = new Models.XPlatInsuranceEntities();
-    }
-
-    // GET api/<controller>
-    public IEnumerable<Models.ClaimDetail> Get()
-    {
-      return ctx.ClaimDetails;
-    }
-
-    // GET api/<controller>/5
-    public Models.ClaimDetail Get(int id)
-    {
-      return ctx.ClaimDetails.Find(id);
     }
 
     // POST api/<controller>
     public void Post([FromBody]Models.ClaimDetail value)
     {
-      ctx.ClaimDetails.Add(value);
-      ctx.SaveChanges();
+      var claims = ClaimsController._MyClaims;
+     var thisClaim = claims.First(c => c.ClaimID == value.ClaimID);
+      thisClaim.ClaimDetails.Add(value);
     }
 
     // PUT api/<controller>/5
     public void Put(int id, [FromBody]Models.ClaimDetail value)
     {
-      ctx.Entry(value).State = EntityState.Modified;
-      ctx.SaveChanges();
+      var claims = ClaimsController._MyClaims;
+      var thisClaim = claims.First(c => c.ClaimID == value.ClaimID);
+      thisClaim.ClaimDetails.Remove(thisClaim.ClaimDetails.First(d => d.ClaimDetailID == value.ClaimDetailID));
+      thisClaim.ClaimDetails.Add(value);
     }
 
-    // DELETE api/<controller>/5
-    public void Delete(int id)
-    {
-      ctx.ClaimDetails.Remove(ctx.ClaimDetails.Find(id));
-    }
   }
 }
